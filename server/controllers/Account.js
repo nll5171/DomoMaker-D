097c311719ddc070ee78d9212a6e9c67950cf3b6
Nick Lang <nll5171@rit.edu>
2024-11-20 18:59:43 -0500

    Changed request for Account controller.

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -70,7 +70,7 @@ const changePass = async (req, res) => {
   try {
     const hash = await Account.generateHash(pass);
     // Gonna try searching by id, can use findOneAndUpdate if fails
-    await Account.findByIdAndUpdate(req.session.account._id, { password: hash }).lean().exec();
+    await Account.findByIdAndUpdate(req.session.account._id, { password: hash });
     return res.json({ redirect: '/maker' });
   } catch (err) {
     return res.status(500).json({ error: 'An error occurred! ' });
