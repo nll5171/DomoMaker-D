26e54efae0e2a6ee363f8e6e3808b5a098f71f93
Nick Lang <nll5171@rit.edu>
2024-11-19 19:38:39 -0500

    Updated Domo controller to return img.

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -32,7 +32,7 @@ const makeDomo = async (req, res) => {
 const getDomos = async (req, res) => {
   try {
     const query = { owner: req.session.account._id };
-    const docs = await Domo.find(query).select('name age').lean().exec();
+    const docs = await Domo.find(query).select('name age img').lean().exec();
 
     return res.json({ domos: docs });
   } catch (err) {
