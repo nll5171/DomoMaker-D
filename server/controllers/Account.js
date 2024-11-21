0de44448afcab569e938f84b13d637f1e2870a27
Nick Lang <nll5171@rit.edu>
2024-11-20 19:33:10 -0500

    More debugging code added.

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -71,7 +71,8 @@ const changePass = async (req, res) => {
     const hash = await Account.generateHash(pass);
     // Gonna try searching by id, can use findOneAndUpdate if fails
     await Account.findByIdAndUpdate(req.session.account._id, { password: hash }).lean().exec();
-    return res.json({ redirect: '/maker' });
+    //return res.json({ redirect: '/maker' });
+    return res.status(201).json();
   } catch (err) {
     return res.status(500).json({ error: 'An error occurred!' });
   }
