c7eb9c6f8452d4eeee21a928a0f5e3c63ec22e0b
Nick Lang <nll5171@rit.edu>
2024-11-21 12:58:50 -0500

    Commented out more code to pass check.

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -68,16 +68,16 @@ const changePass = async (req, res) => {
 
   // attempt to change password
   try {
-    const hash = await Account.generateHash(pass);
+    //const hash = await Account.generateHash(pass);
     // Gonna try searching by id, can use findOneAndUpdate if fails
-    //await Account.findByIdAndUpdate(req.session.account._id, { password: hash }).lean().exec();
-    //return res.json({ redirect: '/maker' });
+    // await Account.findByIdAndUpdate(req.session.account._id, { password: hash }).lean().exec();
+    // return res.json({ redirect: '/maker' });
 
     const query = { _id: req.session.account._id };
     const docs = await Account.find(query).select('username').lean().exec();
 
     return res.json({ account: docs });
-    //return res.status(204);
+    // return res.status(204);
   } catch (err) {
     return res.status(500).json({ error: 'An error occurred!' });
   }
